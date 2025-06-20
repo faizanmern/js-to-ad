1f3a3cefd6a5ddb3a6cd3e5a2fd79df575210aa1
Muhammad Faizan <75075377+faizanmern@users.noreply.github.com>
2025-06-20 17:24:52 +0000

    Add index.js with a function to sum two numbers

---
+++ index.js
@@ -0,0 +1,10 @@
+var num1 = 10;
+var num2 = 20;
+function addNumbers(a, b) {
+    return a + b;
+}
+function main() {
+    var result = addNumbers(num1, num2);
+    console.log("The sum of " + num1 + " and " + num2 + " is: " + result);
+}
+main();     
