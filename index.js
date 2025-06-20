0ac9ad4a323d6cf5f7cea05568164f88f0b365af
Muhammad Faizan <75075377+faizanmern@users.noreply.github.com>
2025-06-20 17:30:31 +0000

    new func

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var num1 = 10;
-var num2 = 20;
-function addNumbers(a, b) {
-    return a + b;
-}
-function main() {
-    var result = addNumbers(num1, num2);
-    console.log("The sum of " + num1 + " and " + num2 + " is: " + result);
-}
-main();     
+// var num1 = 10;
+// var num2 = 20;
+// function addNumbers(a, b) {
+//     return a + b;
+// }
+// function main() {
+//     var result = addNumbers(num1, num2);
+//     console.log("The sum of " + num1 + " and " + num2 + " is: " + result);
+// }
+// main();     
+
+var num = 2
+var num2 = 30.90
+
+console.log(typeof (num))
+console.log(typeof (num2))
+
+var str = "Hello, World!"
+var str2 = "JavaScript is fun!"
+
+console.log(typeof (str))
+console.log(typeof (str2))
+
+console.log(parseFloat(num2))
