a3f2fcff62642f127de57d91bbad4f304bc6dc54
Muhammad Faizan <75075377+faizanmern@users.noreply.github.com>
2025-06-21 21:02:00 +0000

    new obj

--- index.js
+++ index.js
@@ -9,22 +9,56 @@
 // }
 // main();     
 
-var num = 2
-var num2 = 30.90
+// var num = 2
+// var num2 = 30.90
 
-console.log(typeof (num))
-console.log(typeof (num2))
+// console.log(typeof (num))
+// console.log(typeof (num2))
 
-var str = "Hello, World!"
-var str2 = "JavaScript is fun!"
+// var str = "Hello, World!"
+// var str2 = "JavaScript is fun!"
 
-console.log(typeof (str))
-console.log(typeof (str2))
+// console.log(typeof (str))
+// console.log(typeof (str2))
 
-console.log(parseFloat(num2))
+// console.log(parseFloat(num2))
 
-if (4 > 5) {
-    console.log("4 is less than 5")
-} else {
-    console.log("4 is not less than 5")
+// if (4 > 5) {
+//     console.log("4 is less than 5")
+// } else {
+//     console.log("4 is not less than 5")
+// }
+
+// if (5 === '5') {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// if (5 != 4) {
+//     console.log("5 is not equal to 4")
+// }
+// else {
+//     console.log("5 is equal to 4")
+// }
+// if (5 !== '5') {
+//     console.log("5 is not strictly equal to '5'")
+// }
+// else {
+//     console.log("5 is strictly equal to '5'")
+// }
+// if (5 > 4) {
+//     console.log("5 is greater than 4")
+// }
+
+var mCar = {
+    "company": "Corolla",
+    "model": "2007",
+    "NumberPlate": "ANK369",
+    "color": "off White",
+    "EngineCapacity": "1100cc",
+    "isAutomatic": false,
+    "features": ["Air Conditioning", "Power Steering", "Anti-lock Braking System"]
 }
+
+console.log(typeof (mCar));
